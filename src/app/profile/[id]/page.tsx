1237ebf95a0d04a538b9accff1f8fd8e3bdbeec3
takatama-ts <k.takatama@ts.ordermade.jp>
2025-02-10 20:24:56 +0900

    add: dynamic metadata

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,18 @@
-import Link from 'next/link';
+import { Metadata, ResolvedMetadata } from 'next';
 
-export default function UserDetailPage({ params }: { params: { id: string } }) {
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const id = (await params).id;
+  return { title: id, description: `${id}'s profile page` };
+}
+
+export default async function UserDetailPage({ params }: Props) {
   return (
     <main>
-      <h1 className="text-center">User {params.id} Profile</h1>
-      <Link href={'/'}>Home</Link>
+      <h1 className="text-center">User {(await params).id} Profile</h1>
     </main>
   );
 }
