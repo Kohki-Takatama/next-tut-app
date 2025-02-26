57c0810b3b1936ee47a834facb3946ca8825d7a4
takatama-ts <k.takatama@ts.ordermade.jp>
2025-02-26 21:08:00 +0900

    change: mix client component and server component

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,10 +1,13 @@
+import ImageViewer from '@/app/components/imageViewer';
+import PokemonData from '@/app/components/pokemonData';
+
 type Props = {
   params: Promise<{ name: string }>;
 };
 
 export default async function PokemonPage({ params }: Props) {
   const pokemonName = (await params).name;
-  let pokemonData: { name: string; id: string; imageUrl: string; type: string } | null = null;
+  let pokemonData: { name: string; id: string; imageUrl: string[]; type: string } | null = null;
   // 大文字入力に対応
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
   if (response.ok) {
@@ -14,7 +17,7 @@ export default async function PokemonPage({ params }: Props) {
       name: responseJson.name.charAt(0).toUpperCase() + responseJson.name.slice(1).toLowerCase(),
       id: responseJson.id,
       type: responseJson.types.map((e: { slot: number; type: { name: string; url: string } }) => e.type.name).join(' / '),
-      imageUrl: responseJson.sprites.front_default,
+      imageUrl: [responseJson.sprites.front_default, responseJson.sprites.back_default],
     };
   }
 
@@ -24,16 +27,8 @@ export default async function PokemonPage({ params }: Props) {
       <h2>Pokémon Name: {pokemonName}</h2>
       {pokemonData ? (
         <>
-          <h3>ID</h3>
-          <p>{pokemonData.id}</p>
-          <h3>Name</h3>
-          <p>{pokemonData.name}</p>
-          <h3>Image</h3>
-          <p>
-            <img src={pokemonData.imageUrl} alt="Pokemon Image" />
-          </p>
-          <h3>Type</h3>
-          <p>{pokemonData.type}</p>
+          <PokemonData data={pokemonData} />
+          <ImageViewer imageUrl={pokemonData.imageUrl} />
         </>
       ) : (
         <p style={{ color: 'red' }}>The Pokémon with that name could not be found.</p>
